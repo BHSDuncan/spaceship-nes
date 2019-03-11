7170c7db68fbd91ae1e0b386801464431cd26a27
Duncan Brown <duncan@bhs-consultants.com>
2019-03-10 22:23:22 -0400

    Basic static starfield background.

--- spaceship.asm
+++ spaceship.asm
@@ -12,6 +12,8 @@ MIRRORING = %0001 ;%0000 = horizontal, %0001 = vertical, %1000 = four-screen
 ;; variables
 ENUM $0001  ;;start variables at ram location 1; at 0, setting #$02 to certain variables seems to mess things up; dig into this later.
   gamestate: dsb 1  ; db 1 means reserve one byte of space
+  pointerLow: .dsb 1
+  pointerHigh: .dsb 1
 ENDE
 
 ;; constants
@@ -76,7 +78,71 @@ LoadPalettesLoop:
   CPX #$10            
   BNE LoadPalettesLoop  ; Branch to LoadPalettesLoop if compare was Not Equal to zero
                         ; if compare was equal to 32, keep going down  
+
+LoadBGPalettes:
+  LDA $2002             ; read PPU status to reset the high/low latch
+  LDA #$3F
+  STA $2006             ; write the high byte of $3F00 address
+  LDA #$00
+  STA $2006             ; write the low byte of $3F00 address
+  LDX #$00              ; start out at 0
+LoadBGPalettesLoop:
+  LDA bgPalette, x        ; load data from address (palette + the value in x)
+                          ; 1st time through loop it will load palette+0
+                          ; 2nd time through loop it will load palette+1
+                          ; 3rd time through loop it will load palette+2
+                          ; etc
+  STA $2007             ; write to PPU
+  INX                   ; X = X + 1
+  CPX #$10            
+  BNE LoadBGPalettesLoop  ; Branch to LoadPalettesLoop if compare was Not Equal to zero
+                        ; if compare was equal to 32, keep going down  
+
+
+LoadBackground:
+  LDA $2002             ; read PPU status to reset the high/low latch
+  LDA #$20
+  STA $2006             ; write the high byte of $2000 address
+  LDA #$00
+  STA $2006             ; write the low byte of $2000 address
   
+  LDA #<bgdat
+  STA pointerLow
+  
+  LDA #>bgdat
+  STA pointerHigh
+  
+  LDX #$04
+  LDY #$00
+  
+LoadBackgroundLoop:
+  LDA (pointerLow), y     ; load data from address
+  STA $2007             ; write to PPU
+  INY
+  BNE LoadBackgroundLoop
+  INC pointerHigh
+  DEX
+  BNE LoadBackgroundLoop  ; Branch to LoadBackgroundLoop if compare was Not Equal to zero
+                        ; if compare was equal to 128, keep going down
+              
+              
+LoadAttribute:
+  LDA $2002             ; read PPU status to reset the high/low latch
+  LDA #$23
+  STA $2006             ; write the high byte of $23C0 address
+  LDA #$C0
+  STA $2006             ; write the low byte of $23C0 address
+  LDX #$00              ; start out at 0
+LoadAttributeLoop:
+  LDA bgattrs, x      ; load data from address (attribute + the value in x)
+  STA $2007             ; write to PPU
+  INX                   ; X = X + 1
+  CPX #$08              ; Compare X to hex $08, decimal 8 - copying 8 bytes
+  BNE LoadAttributeLoop  ; Branch to LoadAttributeLoop if compare was Not Equal to zero
+                        ; if compare was equal to 128, keep going down
+
+;;;;;;;;;;;;;;;;;;;;
+
 JSR InitSineTable
 JSR InitCosineTable
 
@@ -104,9 +170,9 @@ NMI:
   STA $4014       ; set the high byte (02) of the RAM address, start the transfer
 
   ;;This is the PPU clean up section, so rendering the next frame starts properly.
-  LDA #%10000000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
+  LDA #%10000000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 0
   STA $2000
-  LDA #%00010000   ; enable sprites, enable background, no clipping on left side
+  LDA #%00011000   ; enable sprites, enable background, no clipping on left side
   STA $2001
   LDA #$00        ;;tell the ppu there is no background scrolling
   STA $2005
@@ -155,6 +221,15 @@ UpdateSprites:
   
 palette:
   .incbin "spaceship.dat"
+
+bgPalette:
+  .db $0f,$00,$10,$30,$0f,$01,$21,$2d,$0f,$06,$16,$26,$0f,$09,$19,$29
+
+bgdat:
+  .incbin "space.nam"
+  
+bgattrs:
+  .incbin "spaceAttr.bin"
 
   .include "math.asm"
   .include "enemies.asm"
